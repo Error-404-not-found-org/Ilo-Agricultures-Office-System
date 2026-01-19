0e67f8da8aab3fac96800f93c41c43a17cff74b8
John Lloyd <cabanigdodong@gmail.com>
2026-01-19 22:08:01 +0800

    db config and tools setup added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import path from "path";
+import { clerkMiddleware } from "@clerk/express";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
 const __dirname = path.resolve();
+
+app.use(clerkMiddleware()); // add auth object under req.auth
 
 const PORT = process.env.PORT || 5000;
 
@@ -23,4 +27,5 @@ if (ENV.NODE_ENV === "production") {
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
